06c2055be943e63786d2c49196161617458bb980
semamama <sema.jose69@gmail.com>
2025-08-03 15:36:26 -0700

    js exercise

---
+++ app.js
@@ -0,0 +1,20 @@
+// console.log("hello po");
+
+function add7(num) {
+  return num + 7;
+}
+
+function multiply(num1, num2) {
+  return num1 * num1;
+}
+
+// a function that capitalizes ONLY the first letter of the strung
+function capitalize(string) {
+  return string[0].toUpperCase() + string.slice(1);
+}
+
+// function to grab the LAST letter
+function lastLetter(str) {
+  let lastL = str.length - 1;
+  return str[lastL];
+}
